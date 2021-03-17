e29e108b7e58973fd14a8e0707ed33dce265f89f
••jQ•• <jacqvinson@gmail.com>
2021-03-17 17:54:45 -0500

    Initialize dependabot/composer/composer/helpers/v2/phpstan/phpstan-0.12.80

---
+++ common/lib/dependabot/pull_request_updater.rb
@@ -0,0 +1,60 @@
+# frozen_string_literal: true
+
+require "dependabot/pull_request_updater/github"
+require "dependabot/pull_request_updater/gitlab"
+
+module Dependabot
+  class PullRequestUpdater
+    class BranchProtected < StandardError; end
+
+    attr_reader :source, :files, :base_commit, :old_commit, :credentials,
+                :pull_request_number, :author_details, :signature_key
+
+    def initialize(source:, base_commit:, old_commit:, files:,
+                   credentials:, pull_request_number:,
+                   author_details: nil, signature_key: nil)
+      @source              = source
+      @base_commit         = base_commit
+      @old_commit          = old_commit
+      @files               = files
+      @credentials         = credentials
+      @pull_request_number = pull_request_number
+      @author_details      = author_details
+      @signature_key       = signature_key
+    end
+
+    def update
+      case source.provider
+      when "github" then github_updater.update
+      when "gitlab" then gitlab_updater.update
+      else raise "Unsupported provider #{source.provider}"
+      end
+    end
+
+    private
+
+    def github_updater
+      Github.new(
+        source: source,
+        base_commit: base_commit,
+        old_commit: old_commit,
+        files: files,
+        credentials: credentials,
+        pull_request_number: pull_request_number,
+        author_details: author_details,
+        signature_key: signature_key
+      )
+    end
+
+    def gitlab_updater
+      Gitlab.new(
+        source: source,
+        base_commit: base_commit,
+        old_commit: old_commit,
+        files: files,
+        credentials: credentials,
+        pull_request_number: pull_request_number
+      )
+    end
+  end
+end
---
+++ python/lib/dependabot/python/file_updater/pyproject_preparer.rb
@@ -0,0 +1,121 @@
+# frozen_string_literal: true
+
+require "toml-rb"
+
+require "dependabot/dependency"
+require "dependabot/python/file_parser"
+require "dependabot/python/file_updater"
+require "dependabot/python/authed_url_builder"
+require "dependabot/python/name_normaliser"
+require "securerandom"
+
+module Dependabot
+  module Python
+    class FileUpdater
+      class PyprojectPreparer
+        def initialize(pyproject_content:, lockfile: nil)
+          @pyproject_content = pyproject_content
+          @lockfile = lockfile
+        end
+
+        def replace_sources(credentials)
+          pyproject_object = TomlRB.parse(pyproject_content)
+          poetry_object = pyproject_object.fetch("tool").fetch("poetry")
+
+          sources = pyproject_sources + config_variable_sources(credentials)
+          poetry_object["source"] = sources if sources.any?
+
+          TomlRB.dump(pyproject_object)
+        end
+
+        def sanitize
+          # {{ name }} syntax not allowed
+          pyproject_content.
+            gsub(/\{\{.*?\}\}/, "something").
+            gsub('#{', "{")
+        end
+
+        # rubocop:disable Metrics/PerceivedComplexity
+        # rubocop:disable Metrics/AbcSize
+        def freeze_top_level_dependencies_except(dependencies)
+          return pyproject_content unless lockfile
+
+          pyproject_object = TomlRB.parse(pyproject_content)
+          poetry_object = pyproject_object["tool"]["poetry"]
+          excluded_names = dependencies.map(&:name) + ["python"]
+
+          %w(dependencies dev-dependencies).each do |key|
+            next unless poetry_object[key]
+
+            poetry_object.fetch(key).each do |dep_name, _|
+              next if excluded_names.include?(normalise(dep_name))
+
+              locked_details = locked_details(dep_name)
+
+              next unless (locked_version = locked_details&.fetch("version"))
+
+              next if locked_details&.dig("source", "type") == "directory"
+
+              if locked_details&.dig("source", "type") == "git"
+                poetry_object[key][dep_name] = {
+                  "git" => locked_details&.dig("source", "url"),
+                  "rev" => locked_details&.dig("source", "reference")
+                }
+              elsif poetry_object[key][dep_name].is_a?(Hash)
+                poetry_object[key][dep_name]["version"] = locked_version
+              else
+                poetry_object[key][dep_name] = locked_version
+              end
+            end
+          end
+
+          TomlRB.dump(pyproject_object)
+        end
+        # rubocop:enable Metrics/AbcSize
+        # rubocop:enable Metrics/PerceivedComplexity
+
+        private
+
+        attr_reader :pyproject_content, :lockfile
+
+        def locked_details(dep_name)
+          parsed_lockfile.fetch("package").
+            find { |d| d["name"] == normalise(dep_name) }
+        end
+
+        def normalise(name)
+          NameNormaliser.normalise(name)
+        end
+
+        def pyproject_sources
+          return @pyproject_sources if @pyproject_sources
+
+          pyproject_sources ||=
+            TomlRB.parse(pyproject_content).
+            dig("tool", "poetry", "source")
+
+          @pyproject_sources ||=
+            (pyproject_sources || []).
+            map { |h| h.dup.merge("url" => h["url"].gsub(%r{/*$}, "") + "/") }
+        end
+
+        def config_variable_sources(credentials)
+          @config_variable_sources ||=
+            credentials.
+            select { |cred| cred["type"] == "python_index" }.
+            map do |c|
+              {
+                "url" => AuthedUrlBuilder.authed_url(credential: c),
+                "name" => SecureRandom.hex[0..3],
+                "default" => c["replaces-base"]
+              }.compact
+            end
+        end
+
+        def parsed_lockfile
+          @parsed_lockfile ||= TomlRB.parse(lockfile.content)
+        end
+      end
+    end
+  end
+end
