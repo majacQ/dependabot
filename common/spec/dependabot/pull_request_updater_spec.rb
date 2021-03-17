c84e005f8737486c876c3b2836c77c2f9b024f23
••jQ•• <jacqvinson@gmail.com>
2021-03-17 17:52:58 -0500

    Initial commit

---
+++ common/spec/dependabot/pull_request_updater_spec.rb
@@ -0,0 +1,81 @@
+# frozen_string_literal: true
+
+require "octokit"
+require "spec_helper"
+require "dependabot/dependency"
+require "dependabot/dependency_file"
+require "dependabot/pull_request_updater"
+
+RSpec.describe Dependabot::PullRequestUpdater do
+  subject(:updater) do
+    described_class.new(
+      source: source,
+      base_commit: base_commit,
+      old_commit: old_commit,
+      files: files,
+      credentials: credentials,
+      pull_request_number: pull_request_number
+    )
+  end
+
+  let(:source) { Dependabot::Source.new(provider: "github", repo: "gc/bp") }
+  let(:files) { [] }
+  let(:base_commit) { "basecommitsha" }
+  let(:old_commit) { "oldcommitsha" }
+  let(:pull_request_number) { 1 }
+  let(:credentials) { [] }
+
+  describe "#update" do
+    context "with a GitHub source" do
+      let(:source) { Dependabot::Source.new(provider: "github", repo: "gc/bp") }
+      let(:dummy_updater) { instance_double(described_class::Github) }
+
+      it "delegates to PullRequestUpdater::Github with correct params" do
+        expect(described_class::Github).
+          to receive(:new).
+          with(
+            source: source,
+            base_commit: base_commit,
+            old_commit: old_commit,
+            files: files,
+            credentials: credentials,
+            pull_request_number: pull_request_number,
+            author_details: nil,
+            signature_key: nil
+          ).and_return(dummy_updater)
+        expect(dummy_updater).to receive(:update)
+        updater.update
+      end
+    end
+
+    context "with a Gitlab source" do
+      let(:source) { Dependabot::Source.new(provider: "gitlab", repo: "gc/bp") }
+      let(:dummy_updater) { instance_double(described_class::Gitlab) }
+
+      it "delegates to PullRequestUpdater::Gitlab with correct params" do
+        expect(described_class::Gitlab).
+          to receive(:new).
+          with(
+            source: source,
+            base_commit: base_commit,
+            old_commit: old_commit,
+            files: files,
+            credentials: credentials,
+            pull_request_number: pull_request_number
+          ).and_return(dummy_updater)
+        expect(dummy_updater).to receive(:update)
+        updater.update
+      end
+    end
+    context "with unsupported source" do
+      let(:source) do
+        Dependabot::Source.new(provider: "unknown", repo: "gc/bp")
+      end
+
+      it "raise an error" do
+        expect { updater.update }.
+          to raise_error(RuntimeError, "Unexpected provider 'unknown'")
+      end
+    end
+  end
+end
