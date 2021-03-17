6f9cbf383aa9b3049bbf813b41a9a15cb9afa107
••jQ•• <jacqvinson@gmail.com>
2021-03-17 17:54:33 -0500

    Initialize jurre/npm-peer-checker-without-lockfile

---
+++ common/lib/dependabot/file_parsers/base.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+module Dependabot
+  module FileParsers
+    class Base
+      attr_reader :dependency_files, :repo_contents_path, :credentials, :source
+
+      def initialize(dependency_files:, repo_contents_path: nil, source:,
+                     credentials: [], reject_external_code: false)
+        @dependency_files = dependency_files
+        @repo_contents_path = repo_contents_path
+        @credentials = credentials
+        @source = source
+        @reject_external_code = reject_external_code
+
+        check_required_files
+      end
+
+      def parse
+        raise NotImplementedError
+      end
+
+      private
+
+      def check_required_files
+        raise NotImplementedError
+      end
+
+      def get_original_file(filename)
+        dependency_files.find { |f| f.name == filename }
+      end
+    end
+  end
+end
